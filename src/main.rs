7369fb8cbbf3260184db65f62e45de398ec88f35
Trevor Wilson <kmdreko@gmail.com>
2021-06-13 02:03:56 -0500

    add mongodb integration

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,58 @@
-use actix_web::{get, App, HttpServer, Responder};
+use std::io::{Error, ErrorKind};
+
+use actix_web::web::{Data, Json};
+use actix_web::{get, App, HttpServer};
+use futures::TryStreamExt;
+use mongodb::{bson, Client, Database};
+use serde::{Deserialize, Serialize};
+use tracing::info;
 use tracing_actix_web::TracingLogger;
 use tracing_subscriber::fmt::format::FmtSpan;
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct Test {}
+
 #[get("/test")]
 #[tracing::instrument]
-async fn test() -> impl Responder {
-    "Hello world!"
+async fn test(db: Data<Database>) -> Result<Json<Vec<Test>>, Error> {
+    let items: Vec<Test> = db
+        .collection("test")
+        .find(bson::doc! {}, None)
+        .await
+        .map_err(|err| Error::new(ErrorKind::Other, err))?
+        .try_collect()
+        .await
+        .map_err(|err| Error::new(ErrorKind::Other, err))?;
+
+    Ok(Json(items))
 }
 
 #[actix_web::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
-        .with_span_events(FmtSpan::ENTER)
+        .with_span_events(FmtSpan::NEW)
         .compact()
         .init();
 
-    HttpServer::new(|| App::new().wrap(TracingLogger::default()).service(test))
-        .bind("127.0.0.1:8080")?
-        .run()
+    let uri = "mongodb://localhost:27017";
+    info!("connecting to db: {}", uri);
+    let db = Client::with_uri_str(uri)
         .await
+        .map_err(|err| Error::new(ErrorKind::Other, err))?
+        .database("dnd");
+
+    // ping the database to ensure connection is established
+    db.run_command(bson::doc! { "ping": 1 }, None)
+        .await
+        .map_err(|err| Error::new(ErrorKind::Other, err))?;
+
+    HttpServer::new(move || {
+        App::new()
+            .data(db.clone())
+            .wrap(TracingLogger::default())
+            .service(test)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
