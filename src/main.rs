811bc1ea06818fbcd92be87e2e51b0632501b019
Trevor Wilson <kmdreko@gmail.com>
2021-06-12 23:29:19 -0500

    add actix-web server and trace logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use tracing_actix_web::TracingLogger;
+use tracing_subscriber::fmt::format::FmtSpan;
+
+#[get("/test")]
+#[tracing::instrument]
+async fn test() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::fmt()
+        .with_span_events(FmtSpan::ENTER)
+        .compact()
+        .init();
+
+    HttpServer::new(|| App::new().wrap(TracingLogger::default()).service(test))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
